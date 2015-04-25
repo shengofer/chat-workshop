11c1256b71b5bec686999624b6c725a90fad118d
shengofer <p.shengofer@gmail.com>
2015-04-25 14:44:45 +0300

    delete db

--- app.js
+++ app.js
@@ -6,26 +6,10 @@ var express           =     require('express'),
     cookieParser      =     require('cookie-parser'),
     bodyParser        =     require('body-parser'),
     config            =     require('./configuration/config'),
-    mysql             =     require('mysql'),
     userModel         =     require('./model/user')
     app               =     express();
 
 
-
-//Define MySQL parameter in Config.js file.
-var connection = mysql.createConnection({
-  host     : config.host,
-  user     : config.username,
-  password : config.password,
-  database : config.database
-});
-
-//Connect to Database only if Config.js parameter is set.
-
-if(config.use_database==='true')
-{
-    connection.connect();
-}
 
 // Passport session setup.
 passport.serializeUser(function(user, done) {
@@ -46,22 +30,6 @@ passport.use(new FacebookStrategy({
   },
   function(accessToken, refreshToken, profile, done) {
     process.nextTick(function () {
-      /*//Check whether the User exists or not using profile.id
-      if(config.use_database==='true')
-      {
-      connection.query("SELECT * from user_info where user_id="+profile.id,function(err,rows,fields){
-        if(err) throw err;
-        if(rows.length===0)
-          {
-            console.log("There is no such user, adding now");
-            connection.query("INSERT into user_info(user_id,user_name) VALUES('"+profile.id+"','"+profile.username+"')");
-          }
-          else
-            {
-              console.log("User already exists in database");
-            }
-          });
-      }*/
 
       return done(null, profile);
     });
